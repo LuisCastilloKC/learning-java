4bf0c5a3bbc2c9cae813aee8f9cb2922e2ce0735
Luis Castillo <mr.luiscastillo@gmail.com>
2022-10-16 23:18:14 -0400

    add second example

--- fundamental/java-fudamental/src/IfelseStatement.java
+++ fundamental/java-fudamental/src/IfelseStatement.java
@@ -12,11 +12,24 @@ public class IfelseStatement {
         */
 
 
-        //example
+        // example 1
         int v1 = 10;
         int v2 = 4;
 
         if(v1 > v2)
         System.out.println("v1 is bigger");
+
+        // Example 2
+
+        int v3 = 10;
+        int v4 = 4;
+
+        if(v3 < v4)
+            System.out.println("v3 is Bigger");
+        else if(v3 < v4)
+            System.out.println("v4 is Bigger");
+        else
+            System.out.println("v3 and v4 are equal");
+
     }
 }
