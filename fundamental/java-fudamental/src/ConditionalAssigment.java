c9247286b82ddaef18de4f114dfbd4565b69fe4a
Luis Castillo <mr.luiscastillo@gmail.com>
2022-10-16 23:04:03 -0400

    conditional assigment

---
+++ fundamental/java-fudamental/src/ConditionalAssigment.java
@@ -0,0 +1,15 @@
+public class ConditionalAssigment {
+    
+    public static void conditionalAssigment(String[] arg){
+
+        // result = condition ? true-value : false-value;
+
+        // Example 1
+        int v1 = 7;
+        int v2 = 5;
+        
+        int vMax = v1 > v2 ? v1 : v2;
+        System.out.println(vMax);
+    }
+
+}
