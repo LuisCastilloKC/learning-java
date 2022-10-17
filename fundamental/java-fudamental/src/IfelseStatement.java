5e044c362b2962f57caf859e3ea3fd5ef71c9fb8
Luis Castillo <mr.luiscastillo@gmail.com>
2022-10-16 23:13:11 -0400

    If else statement

---
+++ fundamental/java-fudamental/src/IfelseStatement.java
@@ -0,0 +1,22 @@
+public class IfelseStatement {
+    
+    public static void ifelseStatement(String[] arg){
+
+        /*  
+        
+        if(condition)
+            true-statement;
+        else
+            false-statement;
+
+        */
+
+
+        //example
+        int v1 = 10;
+        int v2 = 4;
+
+        if(v1 > v2)
+        System.out.println("v1 is bigger");
+    }
+}
